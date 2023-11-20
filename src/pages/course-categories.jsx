0f6a84f19ab0535c6ca3728533e253b6ebbe44a5
sanaz-git <szeraati69@gmail.com>
2023-11-20 23:11:53 +0330

    implement pagination

--- src/pages/course-categories.jsx
+++ src/pages/course-categories.jsx
@@ -1,48 +1,43 @@
-import { Suspense } from "react";
 import { Await, defer, useLoaderData } from "react-router-dom";
 import { httpInterceptedService } from "@core/http-service";
-import CategoriesList from "../features/categories/components/category-list";
+import { Suspense } from "react";
 import CategoryList from "../features/categories/components/category-list";
 
 const CourseCategories = () => {
   const data = useLoaderData();
   return (
-    <div className="row">
-      <div className="col-12">
-        <div className="d-flex align-items-center justify-content-between mb-5">
-          <a className="btn btn-primary fw-border">افزودن دسته جدید</a>
+    <>
+      <div className="row">
+        <div className="col-12">
+          <Suspense
+            fallback={<p className="text-info">در حال دریافت اطلاعات...</p>}
+          >
+            <Await resolve={data.categories}>
+              {(loadedCategories) => (
+                <CategoryList categories={loadedCategories} />
+              )}
+            </Await>
+          </Suspense>
         </div>
-        <Suspense
-          fallback={<p className="text-info">در حال دریافت اطلاعات...</p>}
-        >
-          <Await resolve={data.categoriesLoader}>
-            {(loadedCategories) => (
-              <CategoriesList categories={loadedCategories} />
-            )}
-          </Await>
-        </Suspense>
-        <Suspense
-          fallback={<p className="text-info">در حال دریافت اطلاعات...</p>}
-        >
-          <Await resolve={data.categories}>
-            {(loadedCategories) => (
-              <CategoryList categories={loadedCategories} />
-            )}
-          </Await>
-        </Suspense>
       </div>
-    </div>
+    </>
   );
 };
 
-export async function categoriesLoader() {
+export async function categoriesLoader({ request }) {
   return defer({
-    categories: loadCategories(),
+    categories: loadCategories(request),
   });
 }
 
-const loadCategories = async () => {
-  const response = await httpInterceptedService.get("/CourseCategory/sieve");
+const loadCategories = async (request) => {
+  const page = new URL(request.url).searchParams.get("page") || 1;
+  const pageSize = 10;
+
+  let url = "/CourseCategory/sieve";
+
+  url += `?page=${page}&pageSize=${pageSize}`;
+  const response = await httpInterceptedService.get(url);
   return response.data;
 };
 
