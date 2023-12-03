b7db5a08e33e09ef232399795c5847ed4a9dc28b
sanaz-git <szeraati69@gmail.com>
2023-12-03 09:19:27 +0330

    edit somthings

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { RouterProvider } from "react-router-dom";
-import Login from "./features/identity/components/login";
-import Register from "./features/identity/components/register";
 import router from "./router";
 import "./core/i18n";
 import { useAppContext } from "./contexts/app/app-context";
@@ -9,24 +7,24 @@ import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 
 function App() {
-    const { theme } = useAppContext();
-    useEffect(() => {
-        const head = document.head;
-        const link = document.createElement("link");
-        link.rel = "stylesheet";
-        link.href = `/css/${theme}.css`;
-        head.appendChild(link);
+  const { theme } = useAppContext();
+  useEffect(() => {
+    const head = document.head;
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = `/css/${theme}.css`;
+    head.appendChild(link);
 
-        return () => {
-            head.removeChild(link);
-        };
-    }, [theme]);
-    return (
-        <>
-            <RouterProvider router={router} />
-            <ToastContainer rtl />
-        </>
-    );
+    return () => {
+      head.removeChild(link);
+    };
+  }, [theme]);
+  return (
+    <>
+      <RouterProvider router={router} />
+      <ToastContainer rtl />
+    </>
+  );
 }
 
 export default App;
