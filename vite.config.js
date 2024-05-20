49328d2c6fedcd3dd319eb7424ec4a4ef3f5cd00
sanaz-git <szeraati69@gmail.com>
2024-05-20 20:14:03 +0330

    update config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
@@ -11,5 +10,4 @@ export default defineConfig({
     },
   },
   plugins: [react()],
-  base: "/admin-dashbord/",
 });
